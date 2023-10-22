42759cbc66ad1cd68b4e0c2ad87bf0ef96a8e697
Margo <alade.margaret@yandex.ru>
2023-10-22 09:46:49 +0300

    Initial commit

---
+++ Task-6.1/calculate.cpp
@@ -0,0 +1,20 @@
+#include "calculate.h"
+int add(int num1, int num2) {
+	return num1 + num2;
+}
+
+int substract(int num1, int num2) {
+	return num1 - num2;
+}
+
+int multiply(int num1, int num2) {
+	return num1 * num2;
+}
+
+int devide(int num1, int num2) {
+	return num1 / num2;
+}
+
+int exponent(int num1, int num2) {
+	return pow(num1, num2);
+}
---
+++ Task-6.1/calculate.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <math.h>
+int add(int num1, int num2);
+int substract(int num1, int num2);
+int multiply(int num1, int num2);
+int devide(int num1, int num2);
+int exponent(int num1, int num2);
